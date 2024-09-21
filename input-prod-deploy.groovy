6c6cdf2b21af2fcc8f88d97bdf513ae07ad0717e
ram-lavanuru <reddylavanuru315@gmail.com>
2024-09-21 21:43:33 +0530

    Create input-prod-deploy.groovy

---
+++ input-prod-deploy.groovy
@@ -0,0 +1,45 @@
+pipeline {
+    agent any
+    stages {
+        stage ('build'){
+            steps {
+                echo "building the app"
+            }
+        }
+        stage ('sonar') {
+            steps {
+                echo "doing sonar"
+            }
+        }
+        stage ('docker build') {
+            steps {
+                echo "creating image"
+            }
+        }
+        stage ('docker push') {
+            steps {
+                echo "pushing to jfrog"
+            }
+        }
+        stage ("deploy to dev") {
+            steps {
+                echo "deploying to dev"
+            }
+        }
+        stage ("deploy to test") {
+            steps {
+                echo "deploying to test"
+            }
+        }
+        stage ("deploy to prod") {
+            input {
+                message "is everything good to deploy to prod ?"
+                ok "yes"
+                submitter "admin"  //Active Directory or jenkins database user name
+            }
+            steps {
+                echo "deploying to prod"
+            }
+        }
+    }
+}
